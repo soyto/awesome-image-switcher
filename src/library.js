fccf870eb2ac8f2299c9c58ef9efa545bb947830
soyto <soytogaming@gmail.com>
2019-01-28 11:58:58 +0100

    (DEV) Setting export

--- src/library.js
+++ src/library.js
@@ -6,3 +6,13 @@ import GhostEffectPlugin from './app/plugins/GhostEffect.plugin';
 import AccordionEffectPlugin from './app/plugins/AccordionEffect.plugin';
 
 import ImageSwitcher from './app/ImageSwitcher';
+
+
+export default {
+  'ImageSwitcher': ImageSwitcher,
+  'SplitStrategy': SplitStrategy,
+  'TwilightEffectPlugin': TwilightEffectPlugin,
+  'GhostEffectPlugin': GhostEffectPlugin,
+  'AccordionEffectPlugin': AccordionEffectPlugin,
+};
+
